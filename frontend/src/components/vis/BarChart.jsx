0b3f942649ed56724660416109b0a6c280720af9
thanhan910 <63280785+thanhan910@users.noreply.github.com>
2023-08-16 18:25:58 +1000

    Created sample bar chart with Plotly

--- frontend/src/components/vis/BarChart.jsx
+++ frontend/src/components/vis/BarChart.jsx
@@ -1,5 +1,24 @@
+import React, { useEffect } from 'react';
+import Plot from 'react-plotly.js';
+
 const BarChart = () => {
-  return <div className="vis-container">BarChart</div>;
+
+    var trace1 = {
+        x: ['Feature A', 'Feature B', 'Feature C', 'Feature D', 'Feature E'],
+        y: [20, 14, 23, 25, 22],
+        marker: {
+            color: ['rgba(204,204,204,1)', 'rgba(222,45,38,0.8)', 'rgba(204,204,204,1)', 'rgba(204,204,204,1)', 'rgba(204,204,204,1)']
+        },
+        type: 'bar'
+    };
+
+    var data = [trace1];
+
+    var layout = {
+        title: 'Least Used Feature'
+    };      
+
+    return <div className="vis-container"><Plot data={data} layout={layout} /></div>;
 };
 
 export default BarChart;
