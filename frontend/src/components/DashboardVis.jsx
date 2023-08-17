3c4531b3cbb729a98e93287bc6b1f74ae581d558
Morgan McConnon <morganrmcconnon@gmail.com>
2023-08-17 15:31:42 +1000

    Updated vis border radius to use px instead of percentage

--- frontend/src/components/DashboardVis.jsx
+++ frontend/src/components/DashboardVis.jsx
@@ -10,9 +10,11 @@ import "../styles/override.css";
 const DashboardVis = () => {
   const ResponsiveGridLayout = WidthProvider(Responsive);
 
+  //Defines initial size and location of vis components in dashboard
+  //vis defines the component that will be mapped in dashboard render
   const items = [
-    { i: "BarChart", vis: <BarChart />, x: 0, y: 0, w: 4, h: 1 },
-    { i: "Map", vis: <Map />, x: 4, y: 0, w: 3, h: 1 },
+    { i: "BarChart", vis: <BarChart />, x: 0, y: 0, w: 3, h: 1 },
+    { i: "Map", vis: <Map />, x: 4, y: 0, w: 3, h: 3 },
   ];
   return (
     <div>
@@ -23,9 +25,11 @@ const DashboardVis = () => {
         breakpoints={{ lg: 1200, md: 996, sm: 768 }}
         cols={{ lg: 12, md: 10, sm: 6 }}
         isResizable={false}
+        //Determines which className controls draggable handle for components
         draggableHandle=".vis-drag-handle"
       >
         {items.map((item) => {
+          //Renders each vis component as defined in items array
           return (
             <div key={item.i} data-grid={{ x: item.x, y: item.y }}>
               {item.vis}
