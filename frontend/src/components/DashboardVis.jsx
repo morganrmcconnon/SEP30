5cc9c7bfa2099df822260960aa208ae0cdbd23b1
thanhan910 <63280785+thanhan910@users.noreply.github.com>
2023-08-17 11:34:57 +1000

    Fixed .vis-container to fit parent and reallow Resizable

--- frontend/src/components/DashboardVis.jsx
+++ frontend/src/components/DashboardVis.jsx
@@ -22,7 +22,7 @@ const DashboardVis = () => {
         layouts={{ lg: items }}
         breakpoints={{ lg: 1200, md: 996, sm: 768 }}
         cols={{ lg: 12, md: 10, sm: 6 }}
-        isResizable={false}
+        isResizable={true}
         draggableHandle=".vis-drag-handle"
       >
         {items.map((item) => {
