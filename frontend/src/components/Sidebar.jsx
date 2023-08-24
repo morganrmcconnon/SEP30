fb9d6abb3e1886b7e741ad7b016ab7bd66c1681e
Morgan McConnon <morganrmcconnon@gmail.com>
2023-08-24 13:39:01 +1000

    Added link to mainpage on logo

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -4,19 +4,22 @@ import file from "../assets/file.svg";
 import information from "../assets/information.svg";
 import settings from "../assets/settings.svg";
 import logo from "../assets/logo.png";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
     <div>
-      <div className="sidebar-logo">
-        <div>
-          <img className="sidebar-logo-image" src={logo} alt="logo" />
+      <Link to="/">
+        <div className="sidebar-logo">
+          <div>
+            <img className="sidebar-logo-image" src={logo} alt="logo" />
+          </div>
+          <div>
+            <h1>SEP30</h1>
+            <p className="text-data">Mental Health Dashboard</p>
+          </div>
         </div>
-        <div>
-          <h1>SEP30</h1>
-          <p className="text-data">Mental Health Dashboard</p>
-        </div>
-      </div>
+      </Link>
       <h3 className="sidebar-title">MAIN MENU</h3>
       <nav>
         <ul className="sidebar-list">
