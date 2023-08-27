3500d6f398a33526986d85b945431ebb7b9b4d47
thanhan910 <63280785+thanhan910@users.noreply.github.com>
2023-08-27 21:26:47 +1000

    Add inplace option and comments for dict functions

--- Backend/utils.py
+++ Backend/utils.py
@@ -1,17 +1,56 @@
-def remove_none_entries(list_of_dicts):
-    for d in list_of_dicts:
-        keys_to_remove = [key for key, value in d.items() if value is None]
-        for key in keys_to_remove:
-            del d[key]
-    return list_of_dicts
+def remove_none_entries(list_of_dicts, inplace=True):
+    """
+    Remove keys with None values from each dictionary in the list.
 
-def complete_dict_entries(list_of_dicts):
+    Parameters:
+    - list_of_dicts (list of dict): List of dictionaries to process.
+    - inplace (bool): If True, modify the input list in-place. If False, return a modified copy.
+
+    Returns:
+    - None if inplace=True, otherwise a modified list of dictionaries.
+    """
+    if inplace:
+        for d in list_of_dicts:
+            # Find keys with None values
+            keys_to_remove = [key for key, value in d.items() if value is None]
+            # Remove keys with None values
+            for key in keys_to_remove:
+                del d[key]
+    else:
+        # Create a new list of dictionaries
+        new_list = [dict(d) for d in list_of_dicts]
+        for d in new_list:
+            # Create a new dictionary with keys filtered
+            d = {key: value for key, value in d.items() if value is not None}
+        return new_list
+
+def complete_dict_entries(list_of_dicts, inplace=True):
+    """
+    Complete missing keys with None values in each dictionary.
+
+    Parameters:
+    - list_of_dicts (list of dict): List of dictionaries to process.
+    - inplace (bool): If True, modify the input list in-place. If False, return a modified copy.
+
+    Returns:
+    - None if inplace=True, otherwise a modified list of dictionaries.
+    """
     all_keys = set()
     for d in list_of_dicts:
         all_keys.update(d.keys())
 
-    for d in list_of_dicts:
-        for key in all_keys:
-            if key not in d:
-                d[key] = None
-    return list_of_dicts
+    if inplace:
+        for d in list_of_dicts:
+            # Complete missing keys with None values
+            for key in all_keys:
+                if key not in d:
+                    d[key] = None
+    else:
+        # Create a new list of dictionaries
+        new_list = [dict(d) for d in list_of_dicts]
+        for d in new_list:
+            # Complete missing keys with None values
+            for key in all_keys:
+                if key not in d:
+                    d[key] = None
+        return new_list
