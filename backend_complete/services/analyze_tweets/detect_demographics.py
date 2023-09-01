1ff34af3d3eecbecd695601da0336afeb52c3612
thanhan910 <63280785+thanhan910@users.noreply.github.com>
2023-09-02 04:32:25 +1000

    Update detect demographics to handle ""

--- backend_complete/services/analyze_tweets/detect_demographics.py
+++ backend_complete/services/analyze_tweets/detect_demographics.py
@@ -107,7 +107,7 @@ def preprocess_user_object_for_m3inference(
     if user_origninal_lang == None or user_origninal_lang not in LANGS:
 
         # Detect language of description and translate to English
-        if use_translator_if_necessary:
+        if user_origninal_description != '' and use_translator_if_necessary:
             translated = TRANSLATOR.translate(user_origninal_description, dest="en")
             user_english_description = translated.text
             user_detected_lang = translated.src
