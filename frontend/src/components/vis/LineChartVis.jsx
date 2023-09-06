d8ed50438e310045e0aef4316b95bb64d3d19223
traceytpdao <101537796+traceytpdao@users.noreply.github.com>
2023-09-06 14:07:55 +1000

    Update LineChartVis.jsx

--- frontend/src/components/vis/LineChartVis.jsx
+++ frontend/src/components/vis/LineChartVis.jsx
@@ -1,35 +1,28 @@
-import * as d3 from "d3";
-import { useState, useRef, useEffect } from "react";
-import arrowAll from "../../assets/arrow-all.svg";
-import ColorVar from "../ColorVar";
-import dotsVertical from "../../assets/dots-vertical.svg";
-import VisHeader from "../VisHeader";
-import {
-  ResponsiveContainer,
-  LineChart,
-  Line,
-  CartesianGrid,
-  XAxis,
-  YAxis,
-} from "recharts";
+import * as d3 from 'd3';
+import { useState, useRef, useEffect } from 'react';
+import arrowAll from '../../assets/arrow-all.svg';
+import ColorVar from '../ColorVar';
+import dotsVertical from '../../assets/dots-vertical.svg';
+import VisHeader from '../VisHeader';
+import { ResponsiveContainer, LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
 const data = [
-  { name: "A", uv: 400 },
-  { name: "B", uv: 300 },
-  { name: "C", uv: 600 },
-  { name: "D", uv: 650 },
-  { name: "E", uv: 300 },
+  { name: 'A', uv: 400 },
+  { name: 'B', uv: 300 },
+  { name: 'C', uv: 600 },
+  { name: 'D', uv: 650 },
+  { name: 'E', uv: 300 },
 ];
 
 const LineChartVis = () => {
   return (
-    <div className="vis-container">
-      <VisHeader title="Line Chart" subtitle="Line Subtitle" />
-      <div className="vis-svg-container">
-        <ResponsiveContainer width="95%" height={400}>
+    <div className='vis-container'>
+      <VisHeader title='Line Chart' subtitle='Line Subtitle' />
+      <div className='vis-svg-container'>
+        <ResponsiveContainer width='95%' height={400}>
           <LineChart data={data}>
-            <Line type="natural" dataKey="uv" stroke={ColorVar.blue} />
-            <CartesianGrid stroke="#868e96" strokeDasharray="5 5" />
-            <XAxis dataKey="name" />
+            <Line type='natural' dataKey='uv' stroke={ColorVar.blue} />
+            <CartesianGrid stroke='#868e96' strokeDasharray='5 5' />
+            <XAxis dataKey='name' />
             <YAxis />
           </LineChart>
         </ResponsiveContainer>
