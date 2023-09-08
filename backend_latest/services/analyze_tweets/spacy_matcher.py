38ef00f7eb4c1ccd49d476ec8022e26d51b63a79
thanhan910 <63280785+thanhan910@users.noreply.github.com>
2023-09-08 14:38:43 +1000

    Update spacy_matcher.py

--- backend_latest/services/analyze_tweets/spacy_matcher.py
+++ backend_latest/services/analyze_tweets/spacy_matcher.py
@@ -1,6 +1,7 @@
 import spacy
 from nltk.stem import WordNetLemmatizer
 from spacy.matcher import Matcher
+import os
 
 
 def filter_tweet(json_data):
@@ -12,10 +13,10 @@ def filter_tweet(json_data):
     # Initialize Matcher
     matcher = Matcher(nlp.vocab)
 
-    file_path = "data\keywords2.txt"  # Replace with the actual path to your file
+    keywords_file_path = "data\keywords2.txt"  # Replace with the actual path to your file
     keywords = set()
 
-    with open(file_path, "r") as keyword_file:
+    with open(keywords_file_path, "r") as keyword_file:
         for line in keyword_file:
             # print(len(line.split()))
             line = line.lower()
@@ -72,3 +73,64 @@ def filter_tweet(json_data):
             result.append(line)
 
     return result
+
+
+
+def create_matcher_model():
+    lemmatizer = WordNetLemmatizer()
+
+    # Load spaCy language model
+    NLP = spacy.load("en_core_web_sm")
+
+    keywords_file_path = "data/keywords2.txt"  # Replace with the actual path to your file
+
+    with open(keywords_file_path, "r") as keyword_file:
+        lines = keyword_file.readlines()
+
+    keywords = set()
+    for line in lines:
+        line = line.lower()
+        if len(line.split()) == 1:
+            line = lemmatizer.lemmatize(line)
+        else:
+            doc = NLP(line) # tokenize
+            filtered_tokens = [token.text for token in doc if not token.is_stop] # remove stop words
+            filtered_tokens.pop() # remove last word
+            cnt = len(filtered_tokens) # count number of tokens
+            if cnt == 0:
+                continue
+            elif cnt == 1:
+                line = lemmatizer.lemmatize(filtered_tokens[0])
+            else:
+                line = " ".join(filtered_tokens)
+        keywords.add(line.strip())
+
+    keyword_patterns = []
+
+    for keyword in keywords:
+        
+        words = keyword.split()
+        
+        if len(words) == 1:
+            patterns = [{"LEMMA": lemmatizer.lemmatize(keyword)}]
+        else:
+            patterns = [{"LEMMA": x} for x in words]
+
+        keyword_patterns.append(patterns)
+
+    # Initialize Matcher
+    matcher = Matcher(NLP.vocab)
+
+    matcher.add("KeywordPattern", keyword_patterns)
+
+    return matcher, NLP
+
+
+
+def text_is_related_to_mental_health(text, matcher : Matcher, nlp : spacy.language.Language):
+    
+    doc = nlp(text)
+    
+    matches = matcher(doc)
+
+    return len(matches) > 0
