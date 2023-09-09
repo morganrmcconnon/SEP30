e838c9665c055df8da6df1064cfcf15a64a21e3f
TutorSan <93431813+TutorSan@users.noreply.github.com>
2023-09-09 21:36:42 +1000

    Updated geojson_file path

--- backend_latest/services/analyze_tweets/detect_polygon_geojson.py
+++ backend_latest/services/analyze_tweets/detect_polygon_geojson.py
@@ -4,7 +4,7 @@ from shapely.geometry import Point
 # Load the country polygons
 
 
-def detect_geojson_ploygon(latitude, longitude, geojson_file = 'countries.geojson', country_name_key_in_properties = 'ADMIN'):
+def detect_geojson_ploygon(latitude, longitude, geojson_file = '../data/countries.geojson', country_name_key_in_properties = 'ADMIN'):
     """
     Given the latitude and longitude in floating digits, return the country name in the geojson file if the point is in the country polygon.
     """    
