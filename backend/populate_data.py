dd0e548aa03f9e62ff9b8f94fd128e113d38c453
thanhan910 <63280785+thanhan910@users.noreply.github.com>
2023-09-24 09:40:34 +1000

    Update populate data date range

--- backend/populate_data.py
+++ backend/populate_data.py
@@ -4,10 +4,9 @@ if __name__ == '__main__':
     from analysis_pipeline import analyze_data_by
 
     # Get the current time, but fix the year to 2022 because the Twitter Stream collection only contains tweets up to 2022
-    starting_time = datetime(2022, 8, 31, 23, 59, 0)
+    starting_time = datetime(2022, 10, 8, 12, 0, 0)
     # Calculate the previous [period] minutes from the starting_time and download the tweets at that time
-    time_period = 10100
-    for _ in range(time_period):
+    while True:
         analyze_data_by(starting_time.year, starting_time.month, starting_time.day, starting_time.hour, starting_time.minute)
-        starting_time -= timedelta(minutes=1)
+        starting_time -= timedelta(day=1)
 
