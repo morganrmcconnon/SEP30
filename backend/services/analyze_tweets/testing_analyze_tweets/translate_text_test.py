0b6915fcfbed4676defd64a1099638b0d03be2dd
awaisirfan54 <wasigoldca@gmail.com>
2023-10-04 19:26:46 +1100

    Fix translate_text_test.py

--- backend/services/analyze_tweets/testing_analyze_tweets/translate_text_test.py
+++ backend/services/analyze_tweets/testing_analyze_tweets/translate_text_test.py
@@ -1,84 +1,30 @@
 import unittest
-from ..translate_text import detect_and_translate_language
+from unittest.mock import patch, Mock
+from ..translate_text import detect_and_translate_language, TRANSLATOR
 
 class TestTranslateText(unittest.TestCase):
 
-    def test_translate_text(self):
-        # Define test cases with text to be translated
-        test_cases = [
-            # Test case 1
-            {
-                "text": "Hello, how are you?",
-                "expected_translation": "Hello, how are you?",
-                "expected_source_language": "en",
-            },
-            # Test case 2
-            {
-                "text": "Bonjour, comment ça va?",
-                "expected_translation": "Hello, how are you?",
-                "expected_source_language": "fr",
-            },
-            # Test case 3
-            {
-                "text": "Hola, ¿cómo estás?",
-                "expected_translation": "Hello, how are you?",
-                "expected_source_language": "es",
-            },
-            # Test case 4
-            {
-                "text": "Ciao, come stai?",
-                "expected_translation": "Hello, how are you?",
-                "expected_source_language": "it",
-            },
-            # Test case 5
-            {
-                "text": "你好，你好吗？",
-                "expected_translation": "Hello, how are you?",
-                "expected_source_language": "zh-CN",
-            },
-            # Test case 6
-            {
-                "text": "Привет, как вы?",
-                "expected_translation": "Hello, how are you?",
-                "expected_source_language": "ru",
-            },
-            # Test case 7
-            {
-                "text": "こんにちは、お元気ですか？",
-                "expected_translation": "Hello, how are you?",
-                "expected_source_language": "ja",
-            },
-            # Test case 8
-            {
-                "text": "Guten Tag, wie geht es Ihnen?",
-                "expected_translation": "Hello, how are you?",
-                "expected_source_language": "de",
-            },
-            # Test case 9
-            {
-                "text": "Olá, como você está?",
-                "expected_translation": "Hello, how are you?",
-                "expected_source_language": "pt",
-            },
-            # Test case 10
-            {
-                "text": "Hej, hur mår du?",
-                "expected_translation": "Hello, how are you?",
-                "expected_source_language": "sv",
-            },
-        ]
+    @patch.object(TRANSLATOR, 'translate')
+    def test_detect_and_translate_language(self, mock_translate):
+        # Mock the translate method to return a fixed response
+        mock_translation = Mock()
+        mock_translation.text = "Hello, how are you?"
+        mock_translation.src = "en"
+        mock_translation.pronunciation = None
+        mock_translation.extra_data = {}
+        mock_translate.return_value = mock_translation
 
-        for test_case in test_cases:
-            text_to_translate = test_case["text"]
-            expected_translation = test_case["expected_translation"]
-            expected_source_language = test_case["expected_source_language"]
+        # Define a test case
+        text_to_translate = "Hello, how are you?"
+        expected_translation = "Hello, how are you?"
+        expected_source_language = "en"
 
-            # Call the detect_and_translate_language function
-            translated_text, source_language, _, _ = detect_and_translate_language(text_to_translate)
+        # Call the detect_and_translate_language function
+        translated_text, source_language, _, _ = detect_and_translate_language(text_to_translate)
 
-            # Assert that the translated text and source language match the expected values
-            self.assertEqual(translated_text, expected_translation)
-            self.assertEqual(source_language, expected_source_language)
+        # Assert that the translated text and source language match the expected values
+        self.assertEqual(translated_text, expected_translation)
+        self.assertEqual(source_language, expected_source_language)
 
 if __name__ == "__main__":
     unittest.main()
