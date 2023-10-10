84ad156246058cc76baebeb60e11a8f08b60a7e9
thanhan910 <63280785+thanhan910@users.noreply.github.com>
2023-10-10 23:35:16 +1100

    Escape errors for npm run build

--- frontend/src/components/KeywordSearch.tsx
+++ frontend/src/components/KeywordSearch.tsx
@@ -1,9 +1,9 @@
-import { useState } from "react";
+// import { useState } from "react";
 import { /* useActionData, */ Form } from "react-router-dom";
 
 import search from "../assets/search.svg";
-import light from "../assets/light.svg";
-import dark from "../assets/dark.svg";
+// import light from "../assets/light.svg";
+// import dark from "../assets/dark.svg";
 
 import "react-grid-layout/css/styles.css";
 import "../styles/override.css";
@@ -11,16 +11,16 @@ import "../styles/override.css";
 import { useSearchContext } from "../contexts/SearchContext";
 
 const KeywordSearch = () => {
-  const [mode, setMode] = useState(true);
+  // const [mode, setMode] = useState(true);
   const { updateFilterOption } = useSearchContext();
 
   // const actionData = useActionData();
 
-  function toggleTheme() {
-    let element = document.body;
-    element.classList.toggle("dark");
-    mode ? setMode(false) : setMode(true);
-  }
+  // function toggleTheme() {
+  //   let element = document.body;
+  //   element.classList.toggle("dark");
+  //   mode ? setMode(false) : setMode(true);
+  // }
 
   function updateKeyword(e: any) {
     e.preventDefault();
